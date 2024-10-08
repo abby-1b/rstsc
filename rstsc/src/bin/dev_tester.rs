7338eeb40deb89aaa5ead292f8904a19bbea6bc5
Abby.041b <abby.041b@gmail.com>
2024-10-07 21:39:13 -0400

    Further implemented SmallVec throughout the project

--- rstsc/src/bin/dev_tester.rs
+++ rstsc/src/bin/dev_tester.rs
@@ -1,4 +1,3 @@
-// use rstsc::small_vec::SmallVec;
 /// This file is used for less rigorous testing during development.
 
 // use rstsc::minify::minify_ast;
@@ -9,13 +8,6 @@ use rstsc::emit::emit_code;
 const SOURCE_TEST: &str = include_str!("./test.ts");
 
 fn main() {
-  // let mut a = vec![ 1i32, 2, 3 ];
-  // let mut v = Vec::new();
-  // v.push(123i32);
-  // v.push(456);
-  // v.append(&mut a);
-  // println!("{:?}", v);
-
   let mut tokens = TokenList::from(SOURCE_TEST);
 
   let ast = get_block(&mut tokens);
